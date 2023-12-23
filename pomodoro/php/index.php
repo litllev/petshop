787e8260cd057dabc6e2b9cab7c4834a6c20397c
litllev <vitorherisonvhp@gmail.com>
2023-12-23 14:28:38 -0300

    começando

--- pomodoro/php/index.php
+++ pomodoro/php/index.php
@@ -1,6 +1,42 @@
 <?php
 
-
-
-
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body class="container-fluid">
+    <header>
+        <nav class="navbar bg-success" data-bs-theme="dark">
+        <div class="container-fluid">
+    <a class="navbar-brand" href="#">Pagina inicial</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+                <li class="nav-item"><a href="" class="nav-link active" aria-current="page">Pagina inicial</a></li>
+                <li>Perfil</li>
+                <li>Novo pomodoro</li>
+            </ul>
+        
+            </div>
+        </nav>
+
+    </header>
+    <main>
+
+
+    </main>
+    
+
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+</body>
+</html>
