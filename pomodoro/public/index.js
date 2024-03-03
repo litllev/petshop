408ea994ccbf4f7ad2fd66ef556db9dd9b7252c6
litllev <vitorherisonvhp@gmail.com>
2024-03-03 10:47:04 -0300

    botões configurados

--- pomodoro/public/index.js
+++ pomodoro/public/index.js
@@ -1,17 +1,41 @@
 
 let segundos = 0
 let minutos = 0
-function cronometro() {
-    // Incrementando os segundos a cada segundo
-    segundos++;
-    if(segundos % 60 ===0 ){
-        minutos++
-        segundos = 0
+var pausado = false
+const pause = document.querySelector('#pausar')
+const start = document.querySelector('#iniciar')
+const stop = document.querySelector('#parar')
+function iniciar(){
+    pausado = false
+    interval = setInterval(async()=>{  
+        if (!pausado) {
+            
+                segundos++
+                if(segundos % 60 ===0 ){
+                    minutos++
+                    segundos = 0
+                }
+               document.querySelector(".display-1").innerText = `${minutos.toString().padStart(2,'0')}:${segundos.toString().padStart(2,'0')}`
+            }
+            
+        }, 1000);
+        pause.style.display = 'block'
+        start.style.display = 'none'
+}
+function pausar() {
+    pausado = true
+    pause.style.display = "none";
+    start.style.display = "block"
+    clearInterval(interval)
     }
+function parar(){
+    clearInterval(interval)
+    segundos = 0
+    minutos = 0
     document.querySelector(".display-1").innerText = `${minutos.toString().padStart(2,'0')}:${segundos.toString().padStart(2,'0')}`
-    
-    // Chamando novamente a função após 1 segundo
-    setTimeout(cronometro, 100);
+    pause.style.display = 'none'
+    start.style.display = 'block'
+
 }
 
 
